5cc0339863a15fde1adaafe7689c035472524187
STeven <steven.lignereux@hotmail.fr>
2020-08-13 23:53:12 +0200

    add music and finish project

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var bg = document.getElementById('particles-js');
 document.addEventListener('keydown', function(event) {
 
     bg.style.backgroundColor = getRandomColor();
+    var key = event.key;
+    playMusic(key);
 
 });
 
@@ -20,3 +22,29 @@ function getRandomColor() {
     return color;
 }
 
+function playMusic(key) {
+    
+    switch (key) {
+        case "z":
+            var audio = new Audio('Kick1.wav');
+            audio.play();
+            break;
+        case "q":
+            var audio = new Audio('kick.mp3');
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio('kick2.mp3');
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio('Clap1.wav');
+            audio.play();
+            break;
+    
+        default:
+            alert('Appuyez sur ZQSD pour jouer de la musique')
+            break;
+    }
+
+}
